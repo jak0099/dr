e0f4956660f3ff24296a63063a6cbf5fc7fb0f45
moto <90884864+jak0099@users.noreply.github.com>
2024-02-09 23:56:24 +0800

    Add files via upload

--- js/UrleBird.js
+++ js/UrleBird.js
@@ -1,16 +1,9 @@
-// 道长 drpy仓库 https://gitcode.net/qq_32394351/dr_py
-// 道长 drpy安卓本地搭建说明 https://code.gitlink.org.cn/api/v1/repos/hjdhnx/dr_py/blob/master/%E5%AE%89%E5%8D%93%E6%9C%AC%E5%9C%B0%E6%90%AD%E5%BB%BA%E8%AF%B4%E6%98%8E.md
-// 道长 drpy写源 模板规则说明 https://gitcode.net/qq_32394351/dr_py#%E6%A8%A1%E6%9D%BF%E8%A7%84%E5%88%99%E8%AF%B4%E6%98%8E
-// 道长 drpy写源 套模模版 https://code.gitlink.org.cn/api/v1/repos/hjdhnx/dr_py/raw/master/js/%E6%A8%A1%E6%9D%BF.js
-// 道长 drpy写源 相关视频教程 https://www.youtube.com/watch?v=AK7cN-fcwm4
-// 道长 drpy写源 写源教学视频 https://t.me/fongmi_offical/54080/63553
-// 海阔下载 https://haikuo.lanzoui.com/u/GoldRiver
-// 影视TV 官方TG Drpy群 https://t.me/fongmi_offical/63689
-// 影视TV 官方TG 下载 https://t.me/fongmi_release
-
+/**
+ * 20240201 测试OK
+ */
 
 var rule = {
-    title:'UrleBird',
+    title:'UrleBird[飞]',
     host:'https://urlebird.com',
     homeUrl:'/trending/',
     url:'fyclass/page/fypage/[fyclass/]',
