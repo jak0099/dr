2590a3187d0335c5205f9c5c5bc618aa6c0d3cda
moto <90884864+jak0099@users.noreply.github.com>
2024-07-21 00:33:52 +0800

    Add files via upload

--- js/影视/348电影网.js
+++ js/影视/348电影网.js
@@ -1,61 +1,76 @@
 var rule = {
-	title: '348电影网',
-	host: 'https://www.348z.com',
-	// url:'/vodshow/id/fyclass/page/fypage.html',
-	url: '/vodshow/id/fyfilter.html',
-	filterable: 1,//是否启用分类筛选,
-	filter_url: '{{fl.cateId}}{{fl.area}}/page/fypage{{fl.year}}',
-	filter: 
-	{
-		"37":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"37"},{"n":"4K电影","v":"38"},{"n":"4K电视剧","v":"39"}]},{"key":"area","name":"地区","value":[{"n":"全部","v":""},{"n":"大陆","v":"/area/大陆"},{"n":"香港","v":"/area/香港"},{"n":"台湾","v":"/area/台湾"},{"n":"美国","v":"/area/美国"},{"n":"法国","v":"/area/法国"},{"n":"英国","v":"/area/英国"},{"n":"日本","v":"/area/日本"},{"n":"韩国","v":"/area/韩国"},{"n":"德国","v":"/area/德国"},{"n":"泰国","v":"/area/泰国"},{"n":"印度","v":"/area/印度"},{"n":"意大利","v":"/area/意大利"},{"n":"西班牙","v":"/area/西班牙"},{"n":"加拿大","v":"/area/加拿大"},{"n":"其他","v":"/area/其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2024","v":"/year/2024"},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"},{"n":"2010","v":"/year/2010"}]}],
-		"1":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"1"},{"n":"动作片","v":"6"},{"n":"喜剧片","v":"7"},{"n":"爱情片","v":"8"},{"n":"科幻片","v":"9"},{"n":"恐怖片","v":"10"},{"n":"剧情片","v":"11"},{"n":"战争片","v":"12"},{"n":"纪录片","v":"20"},{"n":"灾难片","v":"21"},{"n":"魔幻片","v":"22"},{"n":"青春片","v":"23"},{"n":"犯罪片","v":"24"},{"n":"悬疑片","v":"25"},{"n":"微电影","v":"26"},{"n":"音乐片","v":"27"},{"n":"B站电影","v":"29"}]},{"key":"area","name":"地区","value":[{"n":"全部","v":""},{"n":"大陆","v":"/area/大陆"},{"n":"香港","v":"/area/香港"},{"n":"台湾","v":"/area/台湾"},{"n":"美国","v":"/area/美国"},{"n":"法国","v":"/area/法国"},{"n":"英国","v":"/area/英国"},{"n":"日本","v":"/area/日本"},{"n":"韩国","v":"/area/韩国"},{"n":"德国","v":"/area/德国"},{"n":"泰国","v":"/area/泰国"},{"n":"印度","v":"/area/印度"},{"n":"意大利","v":"/area/意大利"},{"n":"西班牙","v":"/area/西班牙"},{"n":"加拿大","v":"/area/加拿大"},{"n":"其他","v":"/area/其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2024","v":"/year/2024"},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"},{"n":"2010","v":"/year/2010"}]}],
-		"2":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"2"},{"n":"国产剧","v":"13"},{"n":"港台剧","v":"14"},{"n":"日韩剧","v":"15"},{"n":"欧美剧","v":"16"},{"n":"B站剧集","v":"49"},{"n":"其他剧","v":"50"}]},{"key":"area","name":"地区","value":[{"n":"全部","v":""},{"n":"内地","v":"/area/内地"},{"n":"韩国","v":"/area/韩国"},{"n":"香港","v":"/area/香港"},{"n":"台湾","v":"/area/台湾"},{"n":"日本","v":"/area/日本"},{"n":"美国","v":"/area/美国"},{"n":"泰国","v":"/area/泰国"},{"n":"英国","v":"/area/英国"},{"n":"新加坡","v":"/area/新加坡"},{"n":"其他","v":"/area/其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2024","v":"/year/2024"},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"},{"n":"2010","v":"/year/2010"}]}],
-		"3":[{"key":"area","name":"地区","value":[{"n":"全部","v":""},{"n":"内地","v":"/area/内地"},{"n":"港台","v":"/area/港台"},{"n":"日韩","v":"/area/日韩"},{"n":"欧美","v":"/area/欧美"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2024","v":"/year/2024"},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"},{"n":"2010","v":"/year/2010"}]}],
-		"4":[{"key":"area","name":"地区","value":[{"n":"全部","v":""},{"n":"国产","v":"/area/国产"},{"n":"日本","v":"/area/日本"},{"n":"欧美","v":"/area/欧美"},{"n":"其他","v":"/area/其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2024","v":"/year/2024"},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"},{"n":"2010","v":"/year/2010"}]}]
-	},
-	filter_def: {
-		1:{cateId:'1'},
-		2:{cateId:'2'},
-		3:{cateId:'3'},
-		4:{cateId:'4'},
-		37:{cateId:'37'}
-	},
-	searchUrl: '/vodsearch/page/fypage/wd/**.html',
-	searchable: 2,//是否启用全局搜索,
-	headers: {//网站的请求头,完整支持所有的,常带ua和cookies
-		'User-Agent': 'PC_UA',
-	},
-	class_parse: '.nav-channel a;a&&Text;a&&href;/(\\d+).html',
-	play_parse: true,
-	lazy: `js:
-		var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
-		var url = html.url;
+    title: '348电影网',
+    host: 'https://www.348z.com',
+    // url:'/vodshow/id/fyclass/page/fypage.html',
+    url: '/vodshow/id/fyfilter.html',
+    filterable: 1,//是否启用分类筛选,
+    filter_url: '{{fl.cateId}}{{fl.area}}/page/fypage{{fl.year}}',
+    filter: 'H4sIAAAAAAAAA+2Xy27TUBCG38XrSM2xc+2SHeIRUBYWeAV0UQFSVUWilISkBdKiNjQ0FSCROi0pdVRuSeT0ZXyp34Lj2Dkzc5BaSw07L/1/4/Hx7zm/7XWFKcv315VHxpqyrDzQnxp3HyoZZUV/YvBjt1H3hxN+/Fx//MyYFa6Ecq0fbPZDmR8wpZqJ5a2+Y3f95uuYFIC0u27TBFIUxG8Mvc0akBIQc9cdTYCUBfE2drwXbSAsCxdqmqQdg9V5jQNn3ERIhUuNT117H5AKDf2NafBxihA0DM72yAJVaBgcvfcOeghp0HDr3LdPEcrBCl8O/PYuQnm4r+l3f++naw/nCLwNPl04ox10Fph7x//WoadxEyshjR63vmro6GF3LffN+PqHDQv6agadeqwuhY2WYkms67jj/TknFbEkerQsbzSlPSJJmDV95x7apCKWhGcX+3JFLM0rrraHckUsiR4fel53QHtEEnh8IveIJXhAv+WKWIKVWv+u1CI93lru+Jj2iCTR41WLu+w2TmgboYp77l36rTO/2aG3LVTYsZ+97Ut+Mr2oUEVd7ZczadOiSMLjtGboq2icRj+ciZ1wnNQs3wdx+7DN0kxAVJOphqkqUxVTJlOGaVamWURZWaKsjGlJpiVMizItYlqQaQHTvEzzmMpeMewVk71i2Csme8WwV0z2imGvmOxVmLyVaiWjqLd8icAa+IZwxibP8XlMw+J5dvB8QCiHt3C4IQGBXd7ADDMDEPgc5iMHweE8yHJlaeThtHx2YdFZr/F6upkiKXncLCJcb469JPF7U6gliN+2xRPHPfpC2wg1DaE0hBKFkIZC6L/uzyiI6LhGEs0jeW+FEo0lWhFJ6TSn08ynObegaZ69T+k0R1Ly98A1s5pmczrNibK5eMsvRA28yd0jP7VaiZIrsw6fbVr6x5v+8aZ/vGmgLTbQqn8B50a4ODgVAAA=',
+    filter_def: {
+        1: {cateId: '1'},
+        2: {cateId: '2'},
+        3: {cateId: '3'},
+        4: {cateId: '4'},
+        37: {cateId: '37'}
+    },
+    searchUrl: '/vodsearch/page/fypage/wd/**.html',
+    searchable: 2,//是否启用全局搜索,
+    headers: {//网站的请求头,完整支持所有的,常带ua和cookies
+        'User-Agent': 'PC_UA',
+    },
+    class_parse: '.nav-channel a;a&&Text;a&&href;/(\\d+).html',
+    play_parse: true,
+    tab_remove: ['蓝光Z', '极速', '极速2'],
+    lazy: `js:
+		let html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
+		let url = html.url;
 		if (html.encrypt == '1') {
 			url = unescape(url)
 		} else if (html.encrypt == '2') {
 			url = unescape(base64Decode(url))
 		}
-		if (/\\.m3u8|\\.mp4/.test(url)) {
+if (/\\.m3u8/.test(url)) {
+            let body = request(url);
+            let lines = body.split('\\n');
+            let m3u8Url = null;
+            for (let line of lines) {
+                line = line.trim();
+                if (line.endsWith('.m3u8')) {
+                    m3u8Url = urljoin(url,line);//获取嵌套M3U8
+                    console.log(m3u8Url);
+                    break;
+                }
+            }
+            input = {
+                jx: 0,
+                url: m3u8Url || url,
+                parse: 0
+            };
+    } else {
 			input = {
-				jx: 0,
-				url: url,
-				parse: 0
-			}
-		} else {
-			input
+                jx: 0,
+                url: url,
+                parse: 0
+            };
 		}
 	`,
-	limit: 6,
-	double: true, // 推荐内容是否双层定位
-	推荐: '.vodlist;*;*;*;*;*',
-	一级: '.pack-ykpack;a&&title;.eclazy&&data-original;.pack-prb&&Text;a&&href',
-	二级: {
-		"title": ".fyy&&Text;.s-top-info-detail&&Text",
-		"img": ".g-playicon&&img&&src",
-		"desc": ".s-top-info-title span&&Text;;;.item-type&&Text;.item-actor:eq(2)&&Text",
-		"content": ".ec-palytcji span&&Text",
-		"tabs": ".swiper-wrapper:eq(1) .channelname",
-		"lists": ".content_playlist:eq(#id) li"
-	},
-	搜索: '.pack-packcover.returl.list-top-b;*;*;*;*',
+    limit: 6,
+    double: true, // 推荐内容是否双层定位
+    推荐: '.vodlist;*;*;*;*;*',
+    一级: '.pack-ykpack;a&&title;.eclazy&&data-original;.pack-prb&&Text;a&&href',
+    二级: {
+        "title": ".fyy&&Text;.s-top-info-detail&&Text",
+        "img": ".g-playicon&&img&&src",
+        "desc": ".s-top-info-title span&&Text;;;.item-type&&Text;.item-actor:eq(2)&&Text",
+        "content": ".ec-palytcji span&&Text",
+        "tabs": `js:
+            TABS = [];
+			let tabs = pdfa(html.replace(/&nbsp;/g,''), '.swiper-wrapper .channelname');
+			tabs.forEach((it) => {
+    TABS.push(pdfh(it, "body&&Text").replace(/\\s*\\([^)]*\\)/,'').replace(/[^\x00-\x7F]/,''));
+});
+		`,
+        "lists": ".content_playlist:eq(#id) li"
+    },
+    搜索: '.pack-packcover.returl.list-top-b;*;*;*;*',
 }
