7f0e5409e324ab329e4d0f69dbca12cc4abf8dbc
moto <90884864+jak0099@users.noreply.github.com>
2024-09-03 13:31:36 +0800

    Add files via upload

---
+++ js/影视/丽青衣[自动].js
@@ -0,0 +1,12 @@
+var rule = {
+    模板: 'mx',
+    title: '丽青衣[自动]',
+    host: 'http://cj.liqinyi.top:666',
+    url: '/index.php/vod/show/id/fyclass/page/fypage.html',
+    class_parse: '',
+    class_name: '电影&剧集&综艺&动漫&短剧',
+    class_url: '1&2&3&4&5',
+    //class_parse: '.grid-items&&li:lt(6);a&&Text;a&&href;.*/(.*?).html',
+    searchUrl: '/index.php/vod/search/page/fypage/wd/**.html',
+   // 搜索: '.module-items .module-search-item;h3&&a&&Text;img&&data-src;.video-serial&&Text;h3&&a&&href',
+}
