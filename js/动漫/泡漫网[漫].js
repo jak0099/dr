da160afabbb8edb50dcb990bc6a678f8bd2b7d79
moto <90884864+jak0099@users.noreply.github.com>
2024-12-05 23:07:06 +0800

    新增

---
+++ js/动漫/泡漫网[漫].js
@@ -0,0 +1,10 @@
+Object.assign(muban.首图2.二级,{
+  tabs: '.nav li',
+});
+var rule = {
+  模板:'首图2',
+  title: '泡漫网[漫]',
+  host: 'https://www.paoman.cc',
+  url: '/vodtype/fyclass-fypage.html',
+  searchUrl: '/vodsearch/**----------fypage---.html',
+}
