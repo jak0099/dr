ac6571138fac006a920175b1b1386efd2c2211fd
moto <90884864+jak0099@users.noreply.github.com>
2024-12-09 19:22:35 +0800

    新增

---
+++ js/影视/泡泡影视.js
@@ -0,0 +1,12 @@
+Object.assign(muban.首图.二级,{
+    tabs: 'ul.nav li',
+    lists: '.myui-content__list:eq(#id)&&li',
+});
+var rule = {
+  模板:'首图',
+  title: '泡泡影视',
+  host: 'https://www.wxnlm.com',
+  url: '/vodtype/fyclass-fypage.html',
+  searchUrl: '/vodsearch/**----------fypage---.html',
+  class_parse: '.myui-header__menu li:gt(0):lt(5);a&&Text;a&&href;.*/(.*?)\.html',
+}
