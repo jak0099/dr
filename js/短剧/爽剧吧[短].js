116c5212bfac8fce2c2c38f2b34c4164477fa3a8
moto <90884864+jak0099@users.noreply.github.com>
2024-12-10 22:46:47 +0800

    新增

---
+++ js/短剧/爽剧吧[短].js
@@ -0,0 +1,10 @@
+Object.assign(muban.海螺3.二级,{
+    title: '.hl-vod-name&&Text;.hl-tag-item a&&Text',
+});
+var rule = {
+  模板:'海螺3',
+  title: '爽剧吧[短]',
+  host: 'https://www.shuangjuba.com',
+  searchUrl: '/index.php/vod/search/page/fypage/wd/**.html',
+  url: '/index.php/vod/type/id/fyclass/page/fypage.html',
+}
