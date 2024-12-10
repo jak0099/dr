46190d93fa13d526c2b930428fe447c3f1c7133b
moto <90884864+jak0099@users.noreply.github.com>
2024-12-10 22:44:30 +0800

    更新

--- js/动漫/咕咕番[漫].js
+++ js/动漫/咕咕番[漫].js
@@ -1,63 +1,36 @@
+//咕咕番发布页：gugu01.cc
 var rule = {
-    title: '咕咕番[漫]',
-    host: 'https://www.gugufan.com',
-    url: '/index.php/api/vod',
-    searchUrl: '/index.php/vod/search/page/fypage/wd/**.html',
-    headers: {
-        'User-Agent': 'PC_UA',
-    },
-    searchable: 2,
-    quickSearch: 0,
-    filterable: 0,
-    class_name: '连载日漫&完结日漫&剧场&特摄',
-    class_url: '6&7&21&23',
-    limit: 6,
-    double: false,
-    play_parse: true,
-    lazy: $js.toString(() => {
-        let html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
-        let url = html.url;
-        if (url) {
-            input = {parse: 0, url: url, header: rule.headers}
-        }
-    }),
-    一级: $js.toString(() => {
-        // eval(getCryptoJS);
-        let t = Math.floor(Date.now() / 1000);
-        let key = CryptoJS.MD5('DS' + t + 'DCC147D11943AF75').toString();
-        let type = MY_CATE;
-        let pg = MY_PAGE;
-        let data = {
-            'type': type,
-            'class': '',
-            'area': '',
-            'lang': '',
-            'version': '',
-            'state': '',
-            'letter': '',
-            'page': pg,
-            'time': t,
-            'key': key
-        };
-        let list = JSON.parse(post(input, {body: data})).list;
-        let d = [];
-        list.forEach(it => {
-            let url = 'https://www.gugufan.com/index.php/vod/detail/id/' + it.vod_id + '.html';
-            d.push({
-                title: it.vod_name,
-                desc: it.vod_remarks,
-                img: it.vod_pic,
-                url: url
-            });
-        });
-        setResult(d);
-    }),
-    二级: {
-        title: 'h3&&Text',
-        img: '.detail-pic&&img&&data-src',
-        content: '#height_limit&&Text',
-        tabs: '.anthology-tab a',
-        lists: '.anthology-list:eq(#id)&&a',
-    },
-    搜索: '.row-right&&.public-list-box;img&&alt;img&&data-src; span&&Text;a&&href'
+  类型:'影视',//影视|听书|漫画|小说
+  title:'咕咕番[漫]',
+  host:'https://www.gugu3.com',
+  url:'/index.php/vod/show/id/fyclass.html',
+  searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
+  searchable:2,
+  quickSearch:0,
+  filterable:0,
+  headers:{
+      'User-Agent':'PC_UA',
+  },
+  timeout:5000,
+  class_parse:'.head-nav li;a&&Text;a&&href;/(\\d+)\.html',
+  play_parse:true,
+  lazy:$js.toString(()=>{
+    input = {parse:1,url:input,js:''};
+  }),
+  double:true,
+  推荐:'.public-list-div a:gt(4);a;.public-list-exp&&title;.lazy&&data-src;.public-list-prb&&Text;a&&href',
+  一级:'.public-list-div a:gt(4);.public-list-exp&&title;.lazy&&data-src;.public-list-prb&&Text;a&&href',
+  二级:{
+    title:'h3&&Text;.gen-search-form span:eq(1)&&Text',
+    img:'.lazy&&data-src',
+    desc:'.slide-info:eq(3)&&Text;.slide-info a&&Text;.slide-info-remarks:eq(2)&&Text;.slide-info:eq(2)&&Text;.slide-info:eq(1)&&Text',
+    content:'.top26&&Text',
+    tabs:'.anthology-tab a',
+    lists:'.anthology-list-play:eq(#id)&&a',
+    tab_text:'body&&Text',
+    list_text:'body&&Text',
+    list_url:'a&&href',
+    list_url_prefix: '',
+  },
+  搜索:'.public-list-box;.thumb-txt&&Text;.lazy&&data-src;.public-list-prb&&Text;a&&href',
 }
---
+++ js/短剧/吉吉短剧[短].js
@@ -0,0 +1,9 @@
+var rule = {
+  模板:'首图',
+  title: '吉吉短剧[短]',
+  host: 'https://www.jjduanju.com',
+  url: '/sort/fyclass/page/fypage.html',
+  searchUrl: '/searcha/page/fypage.html',
+  class_parse: '.myui-header__list li;a&&Text;a&&href;.*/(\\d+).html',
+  //搜索验证标识:'系统安全验证|请输入验证码',
+}
