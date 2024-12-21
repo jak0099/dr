dbe06b51d2ed23a42cfe3fc256e88f0f7b8146c3
moto <90884864+jak0099@users.noreply.github.com>
2024-12-21 19:43:26 +0800

    同步

--- js/动漫/囧次元动漫[漫].js
+++ js/动漫/囧次元动漫[漫].js
@@ -2,6 +2,18 @@ var rule = {
   模板:'mxpro',
   title: '囧次元动漫[漫]',
   host: 'https://www.9ciyuan.com',
-  url: '/index.php/vod/show/id/fyclass/page/fypage.html',
+  //url: '/index.php/vod/show/id/fyclass/page/fypage.html',//不带筛选写法
   searchUrl: '/index.php/vod/search/page/fypage/wd/**.html',
+  url: '/index.php/vod/show/id/fyclass/fyfilter/page/fypage.html',//带筛选写法
+  filter:'H4sIAAAAAAAAA+3Xy0pCURQG4FfZnHHB2sfMaqZ2M817dsOBxYEkMzALRJxFNKkgAp8ibCBBRm+jUm/ROWWrtVZOm62Zfv8u5WeffmobxwVnyeybtnGOvZb/0ml5lYYzY5x65cQL3o9e+sPXt0AuKrVz7+d0PchccOe+EnxjOjM0DdE0JFOXpq5MLU2tTIGmIFK7SFL/jUgXaLog0whNIzKdp+m8TMM0DcuUdmVlV5Z2ZWVXlnZlZVeWdmVlV5Z2ZWVXQLsC2RXQrkB2BbQrkF0B7QpkV0C7AtkV0K4g6MqUJycm97TmNZsev6mP3XHvZvpNjeKvi7JPiqHHmMfR48yX0ZeZr6CvMF9FX2W+hr7GfB19nXkCPcF8A32DeRI9yTyFnmK+ib7JPI2eZp5BzzDPomeZ59BzzPPoeeYF9ALzInqR+Rb6FvMSeon5Nvo28x30Hea76LvM99D3mMPs72MVvP57fw9a9O6Ob+9Hg7vpd3fcff7o9smJAJtV/wfpJw4Hg/HTgzh1VG2esVPvvcvR9ZU4dXZ42vAmX9GUfZ3TTdBN0E3QTdBN0E2YbIL/51ZHQUdBR0FHQUdBR+F7FEK6CboJugm6CboJugk//yi4YhT0YdOHTR+2f3jYTOcTUcYHvcEbAAA=',
+  filter_url:'/by/{{fl.by}}/letter/{{fl.letter}}/year/{{fl.year}}',
+  //https://www.9ciyuan.com/index.php/vod/show/by/hits/id/4/letter/D/page/2/year/2024.html
+  filter_def:{
+    3: {cateId: '3'},
+    4: {cateId: '4'},
+    20: {cateId: '20'},
+    21: {cateId: '21'},
+    22: {cateId: '22'}
+  },
+  filterable:1,
 }
--- js/动漫/泡漫网[漫].js
+++ js/动漫/泡漫网[漫].js
@@ -5,6 +5,18 @@ var rule = {
   模板:'首图2',
   title: '泡漫网[漫]',
   host: 'https://www.paoman.cc',
-  url: '/vodtype/fyclass-fypage.html',
+  //url: '/vodtype/fyclass-fypage.html',
   searchUrl: '/vodsearch/**----------fypage---.html',
+  url: '/vodshow/fyclass-fyfilter.html',//带筛选写法
+  filter:'H4sIAAAAAAAAA+2Wa0sbQRSG/8oyny3MJvHSfPN+v98tfpASqLS1UG2hSKAQI4nYNEpptLG1QospNGjbILip9c9kN/FfdM2sZ86Z+AcC51ve5z07M7yZZN9NS4SkiFqPrE1LPI298T8KN33qJZKixRJrK89jt8TbTWv4euXZq9jdI2t1O5H0to7rHpJWvAUmqqd77mUZJgJJJhLF2slbPaEknvAyhWp2W++iJJnIfqn+3NMTSuIJd3v/5vAHTAQST9Syn2uZNEwEkuzy9cTNF/UuSpJddgqVqyO9i5JkjSPHSx3oNZQk57jO+s/pcyhJ1kgdVBx90kCSc5z7MZf0OZQ0J77/IRO+JN/Lt3+Vv5/096IkWSNz7Kbyeg0l8UTF+e0WszARSLJLfsc7dPQuStJMf7nXOZRpXZKJ5EWl/FFPKGnFreVg6u56X5Yq5SvzegNsvN4hGQrBsnWBt/WBjV3bdCV2peHaD5HrC8PtwG6H6bZjt91027DbZrqt2G013Qh2I6Ybxm7YdHFWtpmVjbOyzaxsnJVtZiVxVtLMSuKspJmVxFlJMyuJs5JmVhJnJWlWXr7k5fQfSiDvuXTFnHf2zrx0ABsvnZss3CT0759s2gm4k/Au4F2EdwPvJrwHeA/hvcB7Ce8D3kd4P/B+wgeADxA+CHyQ8CHgQ4QPAx8mfAT4COGjwEcJHwM+Rvg48HHCJ4BPED4JfJLwKeBThE8DnyZ8BvgM4bPAZwmfAz5H+DzwecIXgC8Qvgh8kfAl4EuEywf6l3f7ufF6e5l913lvXG8N76kMuYubnH41baz6T9FXhuOdfwD/yerGOn09nm25KV0G1h+/eBkLzmUt+9T/G+ZSw6UGSS41gksNlxouNVxquNREm7PUhASXGi41SHKpEVxquNRwqeFSw6Um2pylJiy41HCpQZJLjeBSw6WGSw2XGi410eYsNRHBpYZLDZJcagSXGi41XGq41HCpiTZfqbHi/wGPqbyuKywAAA==',
+  filter_url:'-{{fl.排序}}-{{fl.剧情}}--{{fl.字母}}---fypage---{{fl.年份}}',
+  //https://www.paoman.cc/vodshow/20--hits-科幻--B---2---2022.html
+  filter_def:{
+    20: {cateId: '20'},
+    21: {cateId: '21'},
+    22: {cateId: '22'},
+    23: {cateId: '23'},
+    24: {cateId: '24'}
+  },
+  filterable:1,
 }
