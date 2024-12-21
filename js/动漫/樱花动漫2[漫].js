9b607ff77cdc9e3b8974769b3588a91c0d77d27a
moto <90884864+jak0099@users.noreply.github.com>
2024-12-21 17:33:05 +0800

    Add files via upload

---
+++ js/动漫/樱花动漫2[漫].js
@@ -0,0 +1,12 @@
+var rule = {
+模板:'首图2',
+  类型:'影视',//影视|听书|漫画|小说
+  title:'樱花动漫2[漫]',
+  host:'https://m.91s.cc/',
+  url:'/vodtype/fyclass-fypage.html',
+  searchUrl:'/vodsearch/**----------2---.html',
+  searchable:2,
+  quickSearch:0,
+  filterable:0,
+  cate_exclude:'新动漫网',
+  }
