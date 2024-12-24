2e7e7271d5c389cb3af6cec03fde12dff871f6c7
moto <90884864+jak0099@users.noreply.github.com>
2024-12-24 23:12:20 +0800

    同步

---
+++ cat/lib/spider.js
@@ -0,0 +1,9 @@
+import * as spider from '%s'
+
+if (!globalThis.__JS_SPIDER__) {
+    if (spider.__jsEvalReturn) {
+        globalThis.__JS_SPIDER__ = spider.__jsEvalReturn()
+    } else if (spider.default) {
+        globalThis.__JS_SPIDER__ = typeof spider.default === 'function' ? spider.default() : spider.default
+    }
+}
