0b03e30ed61bb819e8a64ccd334a5de7d8ab957e
moto <90884864+jak0099@users.noreply.github.com>
2025-01-10 13:35:23 +0800

    Add files via upload

---
+++ js/短剧/火鸟短剧[短].js
@@ -0,0 +1,10 @@
+Object.assign(muban.首图2.二级,{
+    tabs: '.stui-pannel__head h3:eq(0)',
+});
+var rule = {
+    模板:'首图2',
+    title: '火鸟短剧[短]',
+    host: 'http://www.taoboo8.com',
+    url: '/d5sot/fyclass-fypage.html',
+    searchUrl: '/x9ksearch/**----------fypage---.html',
+}
