65af8e93d0dbe726fbe0a0fe38e15c613c5cbf27
moto <90884864+jak0099@users.noreply.github.com>
2025-01-10 13:35:58 +0800

    Add files via upload

---
+++ js/影视/短剧网.js
@@ -0,0 +1,12 @@
+Object.assign(muban.首图2.二级,{
+    tabs: '.stui-pannel__head li',
+});
+var rule = {
+    模板:'首图2',
+    title: '短剧网',
+    host: 'https://www.hbtdhd.com',
+    url: '/vodtype/fyclass-fypage/',
+    searchUrl: '/vodsearch/**----------fypage---/',
+    class_parse: '.stui-header__menu li:gt(0):lt(7);a&&Text;a&&href;.*/(\\d+)/',           
+    搜索验证标识:'系统安全验证',
+}
