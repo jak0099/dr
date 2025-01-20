7ef4682eafd047e988e8c4fdc1734da972532360
moto <90884864+jak0099@users.noreply.github.com>
2025-01-20 17:09:03 +0800

    创建 可可影视3.js

---
+++ js/影视/可可影视3.js
@@ -0,0 +1,11 @@
+muban.首图2.二级.tabs ='.dropdown-menu li';
+muban.首图2.二级.img ='img&&src';
+var rule = {
+  类型:'影视',//影视|听书|漫画|小说
+  模板:'首图2',
+  title:'可可影视3',
+  host:'https://www.oichaw.com',
+  url:'/vodtype/fyclass-fypage.html',
+  searchUrl:'/vodsearch/**----------fypage---.html',
+  cate_exclude:'伦理', 
+}
