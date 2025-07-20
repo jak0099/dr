4eff160ab90ca404756267f12453cc8872dab1ea
moto <90884864+jak0099@users.noreply.github.com>
2025-07-20 10:51:45 +0800

    Add files via upload

---
+++ js/短剧/甜圈短剧[短].js
@@ -0,0 +1,87 @@
+var rule = {
+    类型: '影视',
+    title: '甜圈短剧[短]',
+    host: 'https://mov.cenguigui.cn',
+    url: '/duanju/api.php?classname=fyclass&offset=fypage',
+    searchUrl: '/duanju/api.php?name=**&page=fypage',
+    searchable: 2,
+    quickSearch: 0,
+    filterable: 0,
+    headers: {
+        'User-Agent': 'MOBILE_UA',
+        'Referer': 'https://mov.cenguigui.cn/'
+    },
+    timeout: 5000,    
+    class_name: '🔥 推荐榜&🎬 新剧&🎬 逆袭&🎬 霸总&🎬 现代言情&🎬 打脸虐渣&🎬 豪门恩怨&🎬 神豪&🎬 马甲&🎬 都市日常&🎬 战神归来&🎬 小人物&🎬 女性成长&🎬 大女主&🎬 穿越&🎬 都市修仙&🎬 强者回归&🎬 亲情&🎬 古装&🎬 重生&🎬 闪婚&🎬 赘婿逆袭&🎬 虐恋&🎬 追妻&🎬 天下无敌&🎬 家庭伦理&🎬 萌宝&🎬 古风权谋&🎬 职场&🎬 奇幻脑洞&🎬 异能&🎬 无敌神医&🎬 古风言情&🎬 传承觉醒&🎬 现言甜宠&🎬 奇幻爱情&🎬 乡村&🎬 历史古代&🎬 王妃&🎬 高手下山&🎬 娱乐圈&🎬 强强联合&🎬 破镜重圆&🎬 暗恋成真&🎬 民国&🎬 欢喜冤家&🎬 系统&🎬 真假千金&🎬 龙王&🎬 校园&🎬 穿书&🎬 女帝&🎬 团宠&🎬 年代爱情&🎬 玄幻仙侠&🎬 青梅竹马&🎬 悬疑推理&🎬 皇后&🎬 替身&🎬 大叔&🎬 喜剧&🎬 剧情',    
+    class_url: '推荐榜&新剧&逆袭&霸总&现代言情&打脸虐渣&豪门恩怨&神豪&马甲&都市日常&战神归来&小人物&女性成长&大女主&穿越&都市修仙&强者回归&亲情&古装&重生&闪婚&赘婿逆袭&虐恋&追妻&天下无敌&家庭伦理&萌宝&古风权谋&职场&奇幻脑洞&异能&无敌神医&古风言情&传承觉醒&现言甜宠&奇幻爱情&乡村&历史古代&王妃&高手下山&娱乐圈&强强联合&破镜重圆&暗恋成真&民国&欢喜冤家&系统&真假千金&龙王&校园&穿书&女帝&团宠&年代爱情&玄幻仙侠&青梅竹马&悬疑推理&皇后&替身&大叔&喜剧&剧情',    
+    play_parse: true,
+    double: true,
+    lazy: $js.toString(() => {
+        input = {
+            parse: 0,
+            url: HOST + '/duanju/api.php?video_id=' + input + '&type=mp4',
+            jx: 0
+        };
+    }),        
+    推荐: $js.toString(() => {
+        let url = HOST + '/duanju/api.php?classname=推荐榜&offset=0';
+        let res = request(url, {headers: rule.headers});
+        let data = JSON.parse(res).data;
+        VODS = [];
+        data.forEach(item => {
+            VODS.push({
+                vod_id: item.book_id,
+                vod_name: item.title,
+                vod_pic: item.cover,
+                vod_remarks: `${item.sub_title}|${item.episode_cnt}集`
+            });
+        });
+    }),    
+    一级: $js.toString(() => {
+        let res = request(input, {headers: rule.headers});
+        let data = JSON.parse(res).data;
+        VODS = [];
+        data.forEach(item => {
+            VODS.push({
+                vod_id: item.book_id,
+                vod_name: item.title,
+                vod_pic: item.cover,
+                vod_remarks: `${item.sub_title}|${item.episode_cnt}集`
+            });
+        });
+    }),    
+    二级: $js.toString(() => {
+        let book_id = orId;
+        let url = HOST + '/duanju/api.php?book_id=' + book_id;
+        let res = request(url, {headers: rule.headers});
+        let item = JSON.parse(res);
+        let playUrls = item.data.map(ep => `${ep.title}$${ep.video_id}`);
+        VOD = {
+            vod_id: book_id,
+            vod_name: item.book_name || item.title,
+            type_name: item.category,
+            vod_pic: item.book_pic || item.cover,
+            vod_content: item.desc,
+            vod_remarks: item.duration,
+            vod_year: item.time,
+            //vod_actor: item.author,
+            vod_play_from: '甜圈短剧',
+            vod_play_url: playUrls.join("#")
+        };
+    }),    
+    搜索: $js.toString(() => {
+        let keyword = input.split('?')[1].split('=')[1];
+        let url = HOST + '/duanju/api.php?classname=' + encodeURIComponent(keyword) + '&offset=0';
+        let res = request(url, {headers: rule.headers});
+        let data = JSON.parse(res).data;
+        VODS = [];
+        data.forEach(item => {
+            VODS.push({
+                vod_id: item.book_id,
+                vod_name: item.title,
+                vod_pic: item.cover,
+                vod_remarks: `${item.sub_title}|${item.episode_cnt}集`
+            });
+        });
+    }),    
+}
