762c98344cb36bed38c5768b671ae5a1c532c8b6
moto <90884864+jak0099@users.noreply.github.com>
2025-08-26 11:26:33 +0800

    Update 六月听书[听].js

--- js/听书/六月听书[听].js
+++ js/听书/六月听书[听].js
@@ -140,7 +140,7 @@ var rule = {
         'Cache-Control': 'no-cache',
         'Pragma': 'no-cache',
         'Range': 'bytes=0-',
-        'Referer': 'http://www.6yueting.com/'
+        'Referer': 'http://m.5weiting.com/'
     },
     class_name: '全部分类&玄幻奇幻&修真武侠&恐怖灵异&古今言情&都市言情&穿越重生&粤语古仔&网游小说&通俗文学&历史纪实&军事&悬疑推理&ebc5系列&官场商战&儿童读物&广播剧&外文原版&评书大全&相声小品&百家讲坛&健康养生&教材&期刊头条&戏曲&脱口秀',
     class_url: 't0&t1&t2&t3&t4&t28&t5&t6&t7&t11&t12&t13&t14&t18&t15&t16&t17&t22&t8&t9&t10&t20&t21&t23&t24&t27',
@@ -156,12 +156,12 @@ var rule = {
             timestamp: (new Date).getTime(),
             sign: hex_md5((new Date).getTime() + T + b + U)
         };
-        let html = request('http://www.6yueting.com/web/index/video_new?code=' + data.code + '&no=' + data.no + '&type=' + data.type + '&timestamp=' + data.timestamp + '&sign=' + data.sign);
+        let html = request('http://m.5weiting.com/web/index/video_new?code=' + data.code + '&no=' + data.no + '&type=' + data.type + '&timestamp=' + data.timestamp + '&sign=' + data.sign);
         let url = unescape(JSON.parse(html).data.videoUrl);
         input = {parse: 0, url: url, header: rule.headers};
     }),
     limit: 6,
-    图片来源: '@Referer=http://m.6yueting.com/@User-Agent=MOBILE_UA',
+    图片来源: '@Referer=http://m.5weiting.com/@User-Agent=MOBILE_UA',
     推荐: '*',
     一级: '.list-wrapper&&.item-link;h2--span&&Text;img&&src;.status&&Text;a&&href',
     二级: {
@@ -172,5 +172,5 @@ var rule = {
         "tabs": ".operate-bar&&.total-num",
         "lists": ".book-list:eq(#id)&&.list-item"
     },
-    搜索: 'js:let d=[];let MY_HOME="http://m.6yueting.com";let html=request(input);html=JSON.parse(html);let list=html.data.content;list.forEach(function(it){d.push({title:it.name.replace(/<.*?>/g,""),img:"http://img.6yueting.com:20001/"+it.coverUrlLocal,url:MY_HOME+"/list/"+it.code,desc:it.cdate,content:it.descXx.replace(/&nbsp;/g,"")})});setResult(d);',
+    搜索: 'js:let d=[];let MY_HOME="http://m.5weiting.com";let html=request(input);html=JSON.parse(html);let list=html.data.content;list.forEach(function(it){d.push({title:it.name.replace(/<.*?>/g,""),img:"http://img.6yueting.com:20001/"+it.coverUrlLocal,url:MY_HOME+"/list/"+it.code,desc:it.cdate,content:it.descXx.replace(/&nbsp;/g,"")})});setResult(d);',
 }
